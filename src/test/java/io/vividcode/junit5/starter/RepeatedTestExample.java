228b5dbd008cc6c67c222ee74f8e2ac145af8999
Fu Cheng <alexcheng1982@gmail.com>
2017-08-14 08:53:18 +1200

    repeated

--- src/test/java/io/vividcode/junit5/starter/RepeatedTestExample.java
+++ src/test/java/io/vividcode/junit5/starter/RepeatedTestExample.java
@@ -1,5 +1,6 @@
 package io.vividcode.junit5.starter;
 
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 
@@ -9,5 +10,15 @@ public class RepeatedTestExample {
   @RepeatedTest(3)
   @DisplayName("simple")
   void simpleTest() {
+  }
+
+  @RepeatedTest(value = 3, name = RepeatedTest.LONG_DISPLAY_NAME)
+  @DisplayName("long")
+  void longDisplayName() {
+  }
+
+  @RepeatedTest(value = 3, name = "{currentRepetition} / {totalRepetitions}")
+  @DisplayName("custom")
+  void customDisplayName() {
   }
 }
